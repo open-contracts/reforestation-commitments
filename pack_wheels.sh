3d090ee1dff47aed26b0e0ed2e27623b1e597ee0
Jonas Metzger <metzgerj@stanford.edu>
2022-01-02 17:20:55 +0100

    packed wheels

--- pack_wheels.sh
+++ pack_wheels.sh
@@ -1,7 +1,8 @@
-cd rain_oracle
+cd rainforest_oracle
 mkdir -p pip_wheels
 pip3 install wheel
-pip3 wheel  --prefer-binary --wheel-dir pip_wheels -r requirements.txt
+#pip3 wheel  --prefer-binary --wheel-dir pip_wheels -r requirements.txt
+pip3 wheel --prefer-binary --wheel-dir pip_wheels -r requirements.txt
 tar -czvf - pip_wheels | split -b 32M - pip_wheels.tar.gz
 rm -rf ./pip_wheels
 cat $(find . -type f | sort) | sha256sum | awk '{print $1}'
