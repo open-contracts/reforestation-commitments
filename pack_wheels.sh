52d30ad5f900856b5821995f9199a52294f8aa0a
Jonas Metzger <metzgerj@stanford.edu>
2022-01-03 12:31:55 +0100

    removed apt dependencies

--- pack_wheels.sh
+++ pack_wheels.sh
@@ -4,8 +4,4 @@ pip3 install wheel
 pip3 wheel --prefer-binary --wheel-dir pip_wheels -r requirements.txt
 tar -czvf - pip_wheels | split -b 32M - pip_wheels.tar.gz
 rm -rf ./pip_wheels
-mkdir -p apt_debs && cd apt_debs
-cat ../requirements.txt | grep apt-get | cut -c 19- | eval apt-get download $(</dev/stdin)
-cd .. && tar -czvf - apt_debs | split -b 32M - apt_debs.tar.gz
-rm -rf ./apt_debs
 cat $(find . -type f | sort) | sha256sum | awk '{print $1}'
--- rainforest_oracle/oracle.py
+++ rainforest_oracle/oracle.py
@@ -1,38 +1,38 @@
 
 import opencontracts
-import os
+import os, geopy
 from earthdata import Auth, DataGranules, Accessor
 from datetime import datetime
 from pyhdf.SD import SD, SDC
 import numpy as np
-import cartopy.io.shapereader as shp
-import shapely.geometry as sgeom
+import matplotlib.path as mpath
 
 with opencontracts.enclave_backend() as enclave:
   yr, mo = enclave.user_input('Year-Month (YY-MM)').split('-')
   yr, mo = int(yr), int(mo)
-
   os.environ["CMR_USERNAME"] = enclave.user_input('Username for NASA Earthdata API:')
   os.environ["CMR_PASSWORD"] = enclave.user_input('Password for NASA Earthdata API:')
+
   auth = Auth()
   auth.login(strategy='environment')
-
   # https://lpdaac.usgs.gov/products/mod13c1v006/
   granules = DataGranules(auth).short_name('MOD13C1').version('006').temporal(
       date_from=datetime(2000+yr, mo, 1).isoformat(),
       date_to=datetime(2000+yr, mo, 1).isoformat()
     )
   Accessor(auth).get(granules.get(), './dl')
-  data = SD('./dl/'+os.listdir('./dl')[0], SDC.READ)
-  ndvi = data.select('CMG 0.05 Deg 16 days NDVI').get()[1600:2500,2000:3000] * 0.0001
+  ndvi = SD('./dl/'+os.listdir('./dl')[0], SDC.READ).select(
+      'CMG 0.05 Deg 16 days NDVI'
+    ).get()[1600:2500,2000:3000] * 0.0001
 
-  countries = shp.Reader(shp.natural_earth(category='cultural', name='admin_0_countries')).records()
-  brazil = list(filter(lambda c:c.attributes['ISO_A3'] == 'BRA', countries))[0].geometry
   x, y = np.meshgrid(np.arange(2000,3000), np.arange(1600,2500))
-  contained = lambda x, y: brazil.contains(sgeom.Point(0.05*(x+0.5)-180, 90-0.05*(y+0.5)))
-  in_brazil = np.vectorize(contained)(x, y)
+  lon, lat = 0.05*(x+0.5)-180, 90-0.05*(y+0.5)
+  with open('brazil.geojson', 'r') as f: brazil = geojson.load(f)['geometry']
+  path = mpath.Path(brazil['coordinates'][-1][0], closed=True)
+  in_brazil = path.contains_points(list(zip(lon.flatten(), lat.flatten()))).reshape(lon.shape)
 
-  rainforest_share = (ndvi > 0.7)[in_brazil].mean()
-  rainforest_km2 = rainforest_share * 8515770    # times size of brazil
+  rainforest_share = (ndvi > 0.8)[in_brazil].mean()
+  rainforest_km2 = rainforest_share * 8500000    # times size of brazil
+
   enclave.print(f'Validated rainforest size of {rainforest_km2} km^2 in {mo}-20{yr}')
   enclave.submit(rainforest_km2, mo, yr, types=('uint256', 'uint8', 'uint8',), function_name='measure_rainforest')
