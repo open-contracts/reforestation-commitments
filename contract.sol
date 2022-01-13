a0e18001bccd0ce982d5e147b4c1bc8de34b52a9
open-contracts <90533597+open-contracts@users.noreply.github.com>
2022-01-13 18:15:21 +0100

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,56 +1,23 @@
 pragma solidity ^0.8.0;
 import "https://github.com/open-contracts/protocol/blob/main/solidity_contracts/OpenContractRopsten.sol";
 
-contract WeatherInsurance is OpenContract {
-
-    struct parameters {
-        uint256 payout;
-        uint256 price;
-        address insurer;
-        bool active;
-    }
-
-    mapping(bytes32 => mapping(address => parameters)) public policy;
+contract ReforestationIncentives is OpenContract {
 
     constructor() {
-        setOracle(this.settle.selector, "any");  // for debugging purposes, allow any oracleID
+        setOracle("any", this.measureRainforest.selector);  // for debugging purposes, allow any oracleID
     }
 
-    function policyID(int8 latitude, int8 longitude, uint8 year, uint8 month, uint8 threshold) public pure returns(bytes32) {
-        return keccak256(abi.encode(latitude, longitude, year, month, threshold));
+    function deposit() public payable {
     }
 
-    function request(bytes32 policyID, uint256 payout) public payable {
-        require(!policy[policyID][msg.sender].active, "Your policy is already active.");
-        policy[policyID][msg.sender].price += msg.value;
-        policy[policyID][msg.sender].payout = payout;
+    function measureRainforest(bytes32 oracleID, uint256 rainforest_km2, uint8 mo, uint8 yr) public payable 
+    checkOracle(oracleID, this.measureRainforest.selector) {
+        uint256 reward = 1 * (rainforest_km2 - 0);
+        PayATwitterAccount(0xc8bf1115EB097A4c33d865Be7040ffde8D4FDeA5).deposit{value:reward}("govbrazil");
     }
 
-    function retract(bytes32 policyID) public {
-        require(!policy[policyID][msg.sender].active, "Your policy is already active.");
-        uint256 payment = policy[policyID][msg.sender].price;
-        policy[policyID][msg.sender].price = 0;
-        payable(msg.sender).transfer(payment);
-    }
+}
 
-    function provide(address beneficiary, bytes32 policyID) public payable {
-        require(!policy[policyID][beneficiary].active, "The policy is already active.");
-        require(msg.value >= policy[policyID][beneficiary].payout, "You did not send enough ETH to provide the insurance.");
-        policy[policyID][beneficiary].active = true;
-        policy[policyID][beneficiary].insurer = msg.sender;
-        uint256 payment = policy[policyID][beneficiary].price;
-        policy[policyID][beneficiary].price = 0;
-        payable(msg.sender).transfer(payment);
-    }
-
-    function settle(bytes32 oracleID, address beneficiary, bytes32 policyID, bool damageOccured)
-    public checkOracle(oracleID, this.settle.selector) {
-        require(policy[policyID][beneficiary].active, "The insurance is not active.");
-        uint256 payout = policy[policyID][beneficiary].payout;
-        if (damageOccured) {
-            payable(beneficiary).transfer(payout);
-        } else {
-            payable(policy[policyID][beneficiary].insurer).transfer(payout);
-        }
-    }
+interface PayATwitterAccount {
+    function deposit(string memory twitterHandle) external payable;
 }
