fed757e4c72b2da1056991e256d16690ee735e81
Open Contracts <90533597+open-contracts@users.noreply.github.com>
2022-01-22 00:19:40 +0100

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -8,13 +8,13 @@ contract ReforestationIncentives is OpenContract {
     }
 
     function deposit() public payable {
-        // todo: allow for uint256 rewardPerKm2 argument 
+        // todo: allow depositor to declare their willingness to pay per km2
     }
 
     function measureRainforest(bytes32 oracleID, uint256 rainforest_km2, uint8 mo, uint8 yr) public 
     checkOracle(oracleID, this.measureRainforest.selector) {
         uint256 reward = 1 * (rainforest_km2 - 0);
-        // todo: optimize payment rule, taking rewardPerKm2 of each depositor into accont
+        // todo: take depositors' willingess to pay into account
         PayATwitterAccount(0xc8bf1115EB097A4c33d865Be7040ffde8D4FDeA5).deposit{value:reward}("govbrazil");
     }
 
