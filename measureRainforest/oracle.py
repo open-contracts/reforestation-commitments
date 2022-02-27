de0e83b493a0b022212a2381a28c8b423a7a0390
Open Contracts <90533597+open-contracts@users.noreply.github.com>
2022-02-26 16:33:06 -0800

    Update oracle.py

--- measureRainforest/oracle.py
+++ measureRainforest/oracle.py
@@ -6,19 +6,16 @@ from pyhdf.SD import SD, SDC
 import numpy as np
 import matplotlib.path as mpath
 
-with opencontracts.enclave_backend() as enclave:
-  yr, mo = enclave.user_input('Enter month in "MM/YY" format:').split('/')
-  yr, mo = int(yr), int(mo)
-  os.environ["CMR_USERNAME"] = enclave.user_input('Username for NASA Earthdata API:')
-  os.environ["CMR_PASSWORD"] = enclave.user_input('Password for NASA Earthdata API:')
+with opencontracts.session() as session:
+  yr = int(session.user_input('Enter the year in which the forest should be measured:').split('/'))
+  date = datetime(2000+yr, 1, 1).isoformat()
+  os.environ["CMR_USERNAME"] = session.user_input('Username for NASA Earthdata API:')
+  os.environ["CMR_PASSWORD"] = session.user_input('Password for NASA Earthdata API:')
   auth = Auth()
   assert auth.login(strategy='environment'), "Invalid Credentials" 
   # Data Info: https://lpdaac.usgs.gov/products/mod13c1v006/
-  enclave.expect_delay(60, "Downloading Amazon Rainforest Satellite Images...")
-  granules = DataGranules(auth).short_name('MOD13C1').version('006').temporal(
-      date_from=datetime(2000+yr, mo, 1).isoformat(),
-      date_to=datetime(2000+yr, mo, 1).isoformat()
-    )
+  session.expect_delay(60, "Downloading Amazon Rainforest Satellite Images...")
+  granules = DataGranules(auth).short_name('MOD13C1').version('006').temporal(date_from=date,date_to=date)
   Accessor(auth).get(granules.get(), './dl')
   ndvi = SD('./dl/'+os.listdir('./dl')[0], SDC.READ).select(
       'CMG 0.05 Deg 16 days NDVI'
@@ -33,5 +30,5 @@ with opencontracts.enclave_backend() as enclave:
   rainforest_share = (ndvi > 0.8)[in_brazil].mean()
   rainforest_km2 = int(rainforest_share * 8500000)    # times size of brazil
 
-  enclave.print(f'Validated rainforest size of {rainforest_km2} km^2 in {mo}-20{yr}')
-  enclave.submit(rainforest_km2, mo, yr, types=('uint256', 'uint8', 'uint8',), function_name='measureRainforest')
+  session.print(f'Validated rainforest size of {rainforest_km2} km^2 in {mo}-20{yr}')
+  session.submit(rainforest_km2, mo, yr, types=('uint256', 'uint8', 'uint8',), function_name='measureRainforest')
